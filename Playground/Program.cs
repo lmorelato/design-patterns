c5852ab76fc9b0bf7663eb7f14127b4de3f63eba
Leandro Morelato <lmorelato@gmail.com>
2019-04-12 23:16:39 -0400

    initial commit

---
+++ Playground/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+namespace Playground
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            WriteLine("Hello World!");
+        }
+    }
+}
