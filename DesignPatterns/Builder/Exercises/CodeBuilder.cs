cb87c494f69063050c1c12c23048cf688239fe51
Leandro Morelato <lmorelato@gmail.com>
2019-04-13 01:19:22 -0400

    builder exercises

---
+++ DesignPatterns/Builder/Exercises/CodeBuilder.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Builder.Exercises
+{
+    public sealed class CodeBuilder
+    {
+        private readonly string _className;
+        private readonly IList<(string Name, string Type)> _classFields;
+        private const int IndentSpaceSize = 4;
+
+        public CodeBuilder(string className)
+        {
+            _className = className ?? throw new ArgumentNullException(nameof(className));
+            _classFields = new List<(string Name, string Type)>();
+        }
+
+        public CodeBuilder AddField(string name, string type)
+        {
+            if (name == null) throw new ArgumentNullException(nameof(name));
+            if (type == null) throw new ArgumentNullException(nameof(type));
+            _classFields.Add((name, type));
+            return this;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"public class {_className}");
+            sb.AppendLine("{");
+            foreach (var f in _classFields)
+            {
+                sb.AppendLine($"{IndentFor(1)}public {f.Type} {f.Name};");
+            }
+            sb.AppendLine("}");
+            return sb.ToString();
+        }
+
+        private static string IndentFor(int n)
+        {
+            return new string(' ', IndentSpaceSize * n);
+        }
+    }
+}
---
+++ Test/Builder/Exercises/CodeBuilderTest.cs
@@ -0,0 +1,32 @@
+﻿using DesignPatterns.Builder.Exercises;
+using FluentAssertions;
+using NFluent;
+using Shouldly;
+using Xunit;
+
+namespace Test.Builder.Exercises
+{
+    public class CodeBuilderTest
+    {
+        [Fact]
+        public void AddField_ValidField_CodeBuilder()
+        {
+            // Arrange
+            var builder = new CodeBuilder("Person")
+                .AddField("Name", "string")
+                .AddField("Age", "int");
+
+            // Act
+            var result = builder.ToString();
+
+            //Assert
+            result.Should().NotBeNull()
+                .And.NotBeEmpty()
+                .And.ContainAll("public class Person", "public string Name;", "public int Age;");
+
+            Check.That(result).IsNotNull()
+                .And.IsNotEmpty()
+                .And.Contains("public class Person", "public string Name;", "public int Age;");
+        }
+    }
+}
